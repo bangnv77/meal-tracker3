f6525c40ffa03d8b1d2df9b1ebdc74a7f0d2748b
Bang Nguyen <bangnv77@yahoo.com>
2016-08-15 21:30:26 -0700

    connect the CaloriePipe to the FoodListComponent, allowing users to filter their list of foods by the calorie count

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,19 +1,28 @@
 import { Component, Input, Output, EventEmitter } from 'angular2/core';
 import { FoodComponent } from './food.component';
 import { NewFoodComponent } from './new-food.component';
+import { CaloriePipe } from './calorie.pipe';
 import { Food } from './food.model';
 
 @Component({
   selector: 'food-list',
   templateUrl: 'app/food-list.component.html',
+  pipes: [CaloriePipe],
   directives: [FoodComponent, NewFoodComponent]
 })
 
 export class FoodListComponent {
   @Input() foodList: Food[];
   @Output() onPassFoodUp: EventEmitter<Food> = new EventEmitter();
+
+  filterCalorie: string;
+
   passNewFoodUp(newFood: Food) {
   console.log("food received");
   this.onPassFoodUp.emit(newFood);
+  }
+
+  onCalorieFilterChange(newFilter: string) {
+    this.filterCalorie = newFilter;
   }
 }
